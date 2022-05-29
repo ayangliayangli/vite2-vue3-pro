bc8769d064e29bc7b4e118868140455e2ccbfa44
a02456 <li.yang@casstime.com>
2022-05-29 10:28:47 +0800

    init .env

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>HomePage ...</div>
+  <div>
+    <div>HomePage ...</div>
+    <div>VITE_APP_MY_VAR: {{ VITE_APP_MY_VAR }}</div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const VITE_APP_MY_VAR = import.meta.env.VITE_APP_MY_VAR;
+</script>
 
 <style></style>
